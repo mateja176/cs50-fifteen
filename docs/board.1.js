0c408f5a57239fb1398ae8f437e617014bb5d36c
Mateja Petrovic <mateja176@gmail.com>
2018-03-31 22:19:18 +0200

    Adds docs folder

---
+++ docs/board.1.js
@@ -0,0 +1,52 @@
+export class Board {
+  constructor ( size ) {
+    this.size = size;
+    for ( let index = 0; index < this.size; index++ ) {
+      this[ index ] = [];
+      for (let i = 0; i < this.size; i++) {
+        this[ index ].push(i + (this.size * index));
+      }
+    }
+    this.zeroRow = 0;
+    this.zeroColumn = 0;
+  }
+  move ( direction ) {
+    switch (direction) {
+      case "left":
+        [this[this.zeroRow][this.zeroColumn], this[this.zeroRow][this.zeroColumn + 1]] = [this[this.zeroRow][this.zeroColumn + 1], this[this.zeroRow][this.zeroColumn]];
+        this.zeroColumn++;
+        break;
+      case "right":
+        [this[this.zeroRow][this.zeroColumn], this[this.zeroRow][this.zeroColumn - 1]] = [this[this.zeroRow][this.zeroColumn - 1], this[this.zeroRow][this.zeroColumn]];
+        this.zeroColumn--;
+        break;
+      case "down":
+        [this[this.zeroRow][this.zeroColumn], this[this.zeroRow + 1][this.zeroColumn]] = [this[this.zeroRow + 1][this.zeroColumn], this[this.zeroRow][this.zeroColumn]];
+        this.zeroRow++;
+        break;
+      case "up":
+        [this[this.zeroRow][this.zeroColumn], this[this.zeroRow - 1][this.zeroColumn]] = [this[this.zeroRow - 1][this.zeroColumn], this[this.zeroRow][this.zeroColumn]];
+        this.zeroRow--;
+        break;
+      default:
+        break;
+    }
+  }
+  moveNeighbour (num) {
+    if (this.zeroColumn + 1 < this.size && this[this.zeroRow][this.zeroColumn + 1] == num) {
+      this.move("left");
+      return num;
+    } else if (this.zeroColumn - 1 >= 0 && this[this.zeroRow][this.zeroColumn - 1] == num) {
+      this.move("right");
+      return num;
+    } else if (this.zeroRow + 1 < this.size && this[this.zeroRow + 1][this.zeroColumn] == num) {
+      this.move("down");
+      return num;
+    } else if (this.zeroRow - 1 >= 0 && this[this.zeroRow - 1][this.zeroColumn] == num) {
+      this.move("up");
+      return num;
+    } else {
+      return "Incorrect tile number";
+    }
+  }
+}
